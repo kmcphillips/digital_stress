6078dea5c7395efd616347e0b78c7d96861a6f1a
Kevin McPhillips <github@kevinmcphillips.ca>
2020-03-21 19:22:28 -0400

    Reorder requires

--- base.rb
+++ base.rb
@@ -16,22 +16,22 @@ logger_file.sync = true
 Log = Logger.new(logger_file)
 Log.level = Logger::INFO
 
+require_relative "lib/dedup"
+require_relative "lib/steam"
+require_relative "lib/azure"
+require_relative "lib/gif"
+
+require_relative "lib/datastore"
+require_relative "lib/recorder"
+
 require_relative "lib/base_command"
 require_relative "lib/base_subcommand"
 require_relative "lib/games_command"
 require_relative "lib/status_command"
-
-require_relative "lib/dedup"
 
 require_relative "lib/base_responder"
 require_relative "lib/simple_responder"
 require_relative "lib/t_minus"
 require_relative "lib/alchemy"
 
-require_relative "lib/steam"
-require_relative "lib/azure"
-require_relative "lib/gif"
-
 require_relative "lib/duck"
-require_relative "lib/datastore"
-require_relative "lib/recorder"
