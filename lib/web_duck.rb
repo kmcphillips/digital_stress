482bdd4fc455b4b6e5817057a7df50a9ff657dc9
Kevin McPhillips <github@kevinmcphillips.ca>
2020-04-27 09:33:41 -0400

    Use a more general message endpoint

--- lib/web_duck.rb
+++ lib/web_duck.rb
@@ -4,23 +4,7 @@ class WebDuck < Sinatra::Application
     attr_accessor :bot
   end
 
-  def channels
-    ENV["WEB_NOTIFY_CHANNELS"]
-      .split(",")
-      .reject(&:blank?)
-      .map(&:to_i)
-      .map { |id| self.class.bot.channel(id) }
-  end
-
-  def notify_channels(messages)
-    channels.each do |channel|
-      Array(messages).each do |message|
-        channel.send_message(message)
-      end
-    end
-  end
-
-  set :port, 4444
+  set :port, ENV["WEB_PORT"]
 
   use Rack::Auth::Basic do |username, password|
     username == ENV["WEB_AUTH_USERNAME"] && password == ENV["WEB_AUTH_PASSWORD"]
@@ -30,26 +14,15 @@ class WebDuck < Sinatra::Application
     'Quack!'
   end
 
-  post '/quigital/new_caller' do
-    phone_number = params["phone_number"]
-    location = params["location"]
-    url = params["url"]
+  post '/message/:server/:channel' do
+    server = params["server"]
+    channel = params["channel"]
+    message = params["message"]
+    channels = self.class.bot.find_channels(channel, server)
 
-    message = ":telephone: New Quigital caller `#{ phone_number }` #{ location } #{ url }"
+    Log.warn("No channels found for #{ server }##{ channel }") if channels.empty?
 
-    notify_channels(message)
-
-    "Quack, ok."
-  end
-
-  post '/quigital/ending' do
-    phone_number = params["phone_number"]
-    location = params["location"]
-    url = params["url"]
-
-    message = ":tada: Quigital ending `#{ phone_number }` #{ location } #{ url }"
-
-    notify_channels(message)
+    channels.each { |channel| channel.send_message(message) }
 
     "Quack, ok."
   end
