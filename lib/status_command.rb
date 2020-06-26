4dc60b256c77d7bc00275e074ef35796248f6e57
Kevin McPhillips <github@kevinmcphillips.ca>
2020-06-26 17:34:52 -0400

    Put the learning first in status

--- lib/status_command.rb
+++ lib/status_command.rb
@@ -9,15 +9,15 @@ class StatusCommand < BaseCommand
       ":duck: on `#{ hostname }` `(#{ ip_address })`",
     ]
 
+    if server && last_learned = Learner.last(server: server)
+      lines << "Learned **#{ Learner.count(server: server) }** things. Last #{ TimeDifference.between(Time.at(last_learned[:timestamp]), Time.now).humanize || 'a second' } ago."
+    end
+
     if Recorder.record_server?(@event) || (@event.channel.pm? && @event.channel.name == "kmcphillips")
       counts = Recorder.counts
       last = Recorder.last
       lines << "Last message by **#{ last[:username] }** #{ TimeDifference.between(Time.at(last[:timestamp]), Time.now).humanize || 'a second' } ago."
       counts.each{ |r| lines << "  **#{ r[:username] }**: #{ r[:count] } messages" }
-    end
-
-    if server && last_learned = Learner.last(server: server)
-      lines << "Learned **#{ Learner.count(server: server) }** things. Last #{ TimeDifference.between(Time.at(last_learned[:timestamp]), Time.now).humanize || 'a second' } ago."
     end
 
     lines.reject(&:blank?)
