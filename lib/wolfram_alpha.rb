56890bad5ae3b442aab7457c731e33c7f8d4e8c7
Kevin McPhillips <github@kevinmcphillips.ca>
2020-06-28 15:14:02 -0400

    Extract did you mean

--- lib/wolfram_alpha.rb
+++ lib/wolfram_alpha.rb
@@ -18,16 +18,21 @@ module WolframAlpha
       Log.error(response)
       ":bangbang: queryresult.error was not false"
     elsif response.dig("queryresult", "success") == "true"
-      extract_reply(response)
+      extract_success_reply(response)
     else
       Log.error(response)
-      ":bangbang: queryresult.success was false"
+
+      if response["queryresult"]["didyoumeans"]
+        extract_didyoumean_reply(response)
+      else
+        ":bangbang: queryresult.success was false and had no guesses"
+      end
     end
   end
 
   private
 
-  def extract_reply(response)
+  def extract_success_reply(response)
     response["queryresult"]["pod"].map do |pod|
       subpods = pod["subpod"]
       subpods = [ subpods ] if subpods.is_a?(Hash)
@@ -35,5 +40,15 @@ module WolframAlpha
 
       "**#{ pod["title"]}** : #{ values.join(', ') }" unless values.blank?
     end.compact
+  end
+
+  def extract_didyoumean_reply(response)
+    result = ["**Did you mean?**"]
+
+    response["queryresult"]["didyoumeans"]["didyoumean"].map do |dym|
+      result << "    #{ dym["__content__"] } (#{ dym["score"].to_f.round(1) * 100 }%)"
+    end
+
+    result
   end
 end
