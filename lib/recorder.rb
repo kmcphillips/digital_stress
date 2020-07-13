cc99bd9ac63557b91e1d3c4ea60fb6d63b84eaae
Kevin McPhillips <github@kevinmcphillips.ca>
2020-07-13 17:12:14 -0400

    Sort by number of messages

--- lib/recorder.rb
+++ lib/recorder.rb
@@ -69,7 +69,7 @@ module Recorder
   end
 
   def counts
-    DB["SELECT DISTINCT user_id, COUNT(*) AS count FROM messages GROUP BY user_id ORDER BY user_id ASC"].all
+    DB["SELECT DISTINCT user_id, COUNT(*) AS count FROM messages GROUP BY user_id ORDER BY count DESC"].all
   end
 
   def last
