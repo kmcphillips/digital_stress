2f658ffe3d3a38cd34ca533a79512e55842e066b
Kevin McPhillips <github@kevinmcphillips.ca>
2020-10-29 09:32:12 -0400

    Allow dumping users to text

---
+++ dump.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require_relative "base"
+
+Recorder.dump.each do |user_id, record|
+  filename = "output/#{ record[:username] }_#{ Time.now.strftime("%F") }.txt"
+
+  File.open(filename, "w") do |f|
+    f.write(record[:messages].join("\n"))
+  end
+
+  puts filename
+end
