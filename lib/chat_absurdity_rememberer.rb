946a91cfcd64172dd0b3cd6f5f253268e327f8da
Kevin McPhillips <github@kevinmcphillips.ca>
2020-10-29 22:13:29 -0400

    First crack at remembering conversation activity

--- lib/chat_absurdity_rememberer.rb
+++ lib/chat_absurdity_rememberer.rb
@@ -4,6 +4,8 @@ class ChatAbsurdityRemberer
     "mandatemandate#general",
     "duck-bot-test#testing",
   ].freeze
+  BACKOFF_SECONDS = 5..45
+  ACTIVE_CONVERSATION_WINDOW_SECONDS = 120
 
   attr_reader :server, :channel
 
@@ -36,10 +38,41 @@ class ChatAbsurdityRemberer
     true
   end
 
+  def note_conversation(user_id:)
+    ids = JSON.parse(KV.read(conversation_key).presence || "[]")
+    ids << user_id
+    ids = ids.uniq
+    KV.write(conversation_key, ids.to_json, ttl: ACTIVE_CONVERSATION_WINDOW_SECONDS)
+    ids
+  end
+
+  def active_conversation?
+    JSON.parse(KV.read(conversation_key).presence || "[]").count >= 2
+  end
+
+  def in_backoff?
+    !!KV.read(backoff_key)
+  end
+
+  def backoff
+    backoff_time = rand(BACKOFF_SECONDS)
+    puts "backoff #{ backoff_time }"
+    Log.info("[#{ self.class }] Backing off for #{ backoff_time } seconds in #{ backoff_key }")
+    KV.write(backoff_key, "true", ttl: backoff_time)
+    true
+  end
+
   private
 
   def enabled_key
     "chat-status-#{ server }-#{ channel }"
   end
 
+  def backoff_key
+    "chat-backoff-#{ server }-#{ channel }"
+  end
+
+  def conversation_key
+    "chat-conversation-#{ server }-#{ channel }"
+  end
 end
--- lib/chat_responder.rb
+++ lib/chat_responder.rb
@@ -4,7 +4,17 @@ class ChatResponder < BaseResponder
     return unless ChatAbsurdityRemberer::CHANNELS.include?("#{ event.server&.name }##{ event.channel&.name }")
     return unless remberer.enabled?
 
-    # TODO
+    remberer.note_conversation(user_id: event.user.id)
+
+    if remberer.active_conversation?
+      if !remberer.in_backoff?
+        remberer.backoff
+        event.respond "Quaaaack!!! :robot:"
+        # event.respond(remberer.consume_message(user_id: event.user.id)) # TODO
+      end
+    else
+      remberer.backoff unless remberer.in_backoff?
+    end
   end
 
   private
