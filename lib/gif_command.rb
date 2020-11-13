1c5171a21e37993756f59e172b8412e562ad7731
Kevin McPhillips <github@kevinmcphillips.ca>
2020-11-13 14:02:23 -0500

    Dedup shouldn't care about the query, just the result

--- lib/gif_command.rb
+++ lib/gif_command.rb
@@ -6,7 +6,7 @@ class GifCommand < BaseCommand
     if search.blank?
       "Quacking-search for something"
     else
-      Dedup.list(Gif.search_urls(search), namespace: ["Gif", event.server&.name, event.channel&.name, search.strip]) || "Quack-all found"
+      Dedup.list(Gif.search_urls(search), namespace: ["Gif", event.server&.name, event.channel&.name]) || "Quack-all found"
     end
   end
 end
