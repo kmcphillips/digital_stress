af8f6900ea4d43c7e5904c689ff565aba50a8d5f
Kevin McPhillips <github@kevinmcphillips.ca>
2020-12-17 12:08:21 -0500

    Kelvin was a bad idea

--- lib/responders/temperature_responder.rb
+++ lib/responders/temperature_responder.rb
@@ -2,7 +2,6 @@
 class TemperatureResponder < BaseResponder
   REGEX_FARENHEIT = /(?:\s|^)((?:-\s?)?\d+(?:\.\d+)?)\s?(?:degrees?\s)?f(?:\b|(?:arenheit))/i
   REGEX_CELCIUS = /(?:\s|^)((?:-\s?)?\d+(?:\.\d+)?)\s?(?:degrees?\s)?c(?:\b|(?:elcius))/i
-  REGEX_KELVIN = /(?:\s|^)((?:-\s?)?\d+(?:\.\d+)?)\s?(?:degrees?\s)?k(?:\b|(?:elvin))/i
   REGEX_DEGREES_WITHOUT_UNIT = /(?:\s|^)((?:-\s?)?\d+(?:\.\d+)?\s?degrees?)(?:$|(?:\s[^fc]))/i
 
   def respond
@@ -12,16 +11,9 @@ class TemperatureResponder < BaseResponder
     elsif match = text.match(REGEX_CELCIUS)
       c = match[0]
       event.respond(":thermometer: #{ round_pretty(c_to_f(c)) } F")
-    elsif match = text.match(REGEX_KELVIN)
-      k = match[0]
-      if k.to_f < 0.0
-        event.respond(":thermometer: Impossible")
-      else
-        event.respond(":thermometer: #{ round_pretty(k_to_c(k)) } C")
-      end
     elsif match = text.match(REGEX_DEGREES_WITHOUT_UNIT)
       d = match[0]
-      event.respond(":thermometer: #{ d } in Celcius or American?")
+      event.respond(":thermometer: #{ d } in Celcius or American??")
     end
   end
 
@@ -33,10 +25,6 @@ class TemperatureResponder < BaseResponder
 
   def c_to_f(input)
     ( input.to_f * 9 / 5 ) + 32
-  end
-
-  def k_to_c(input)
-    input.to_f - 273.15
   end
 
   def round_pretty(input)
