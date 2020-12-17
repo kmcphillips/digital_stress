21d412b332cd3cbb5e88edb473f50da559da4cd0
Kevin McPhillips <github@kevinmcphillips.ca>
2020-12-17 09:47:05 -0500

    Bump duck mod to 0.0.3

--- factorio_mod/duck/control.lua
+++ factorio_mod/duck/control.lua
@@ -7,7 +7,7 @@ script.on_event(defines.events.on_player_died,
     local file_name = "duck/on_player_died_" .. game.tick
 
     if cause.name == "locomotive" or cause.name == "fluid-wagon" or cause.name == "cargo-wagon" then
-      game.write_file(file_name, file_contents, false) -- 0
+      game.write_file(file_name, file_contents, false, 0)
       game.print("duck: 0 DAYS SINCE A TRAIN ACCIDENT")
     else
       game.print("duck: heat")
