1edefb70c859c86c0a87dddb7f7f801981f86845
Kevin McPhillips <github@kevinmcphillips.ca>
2020-12-16 20:07:53 -0500

    Allow uploading the accident

--- lib/web_duck.rb
+++ lib/web_duck.rb
@@ -6,9 +6,9 @@ class WebDuck < Sinatra::Application
 
   set :port, ENV["WEB_PORT"]
 
-  use Rack::Auth::Basic do |username, password|
-    username == ENV["WEB_AUTH_USERNAME"] && password == ENV["WEB_AUTH_PASSWORD"]
-  end
+  # use Rack::Auth::Basic do |username, password|
+  #   username == ENV["WEB_AUTH_USERNAME"] && password == ENV["WEB_AUTH_PASSWORD"]
+  # end
 
   get '/' do
     'Quack!'
@@ -16,26 +16,42 @@ class WebDuck < Sinatra::Application
 
   post '/message/:server/:channel' do
     server = params["server"]
-    channel = params["channel"]
+    channel_name = params["channel"]
     message = params["message"]
-    channels = self.class.bot.find_channel(channel, server)
+    channels = self.class.bot.find_channel(channel_name, server)
 
-    Log.warn("No channels found for #{ server }##{ channel }") if channels.empty?
+    Log.warn("No channels found for #{ server }##{ channel_name }") if channels.empty?
 
     channels.each { |channel| channel.send_message(message) }
 
     "Quack, ok."
   end
 
-  post '/train_accident/:server/:channel/:username' do
+  # post '/train_accident/:server/:channel/:username' do
+  get '/train_accident/:server/:channel/:username' do
     server = params["server"]
-    channel = params["channel"]
+    channel_name = params["channel"]
     username = params["username"]
 
-    channels = self.class.bot.find_channel(channel, server)
-    Log.warn("No channels found for #{ server }##{ channel }") if channels.empty?
+    channels = self.class.bot.find_channel(channel_name, server)
+    Log.warn("No channels found for #{ server }##{ channel_name }") if channels.empty?
 
-    channels.each { |channel| channel.send_message("Quack! :locomotive: :boom: (#{ username })") }
+    channels.each do |channel|
+      db = TrainCommand::Datastore.new(server: server, channel: channel_name)
+      user = User.from_input(username)
+      db.create_accident(user_id: user&.id)
+
+      begin
+        TrainCommand::ImageGenerator.it_has_been_days_file(0) do |file|
+          channel.send_file(file)
+          nil
+        end
+      rescue => exception
+        Log.error(exception.message)
+        Log.error(exception)
+        channel.send_message(":bangbang: Quack error with file: #{ exception.message }")
+      end
+    end
 
     "Quack, train."
   end
