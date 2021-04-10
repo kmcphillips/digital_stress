58dca4f8fe88d5f299ab43893069de14ffba6332
Kevin McPhillips <github@kevinmcphillips.ca>
2021-04-10 09:59:36 -0400

    For count of sent announcements

--- lib/tasks/daily_announcements.rb
+++ lib/tasks/daily_announcements.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 class DailyAnnouncements < TaskBase
   def run
+    count = 0
+
     Global.config.servers.each do |server_name, server_config|
       if server_config.tasks&.daily_announcements
         server_config.tasks.daily_announcements.each do |task_config|
           if Date.today.day == task_config.day.to_i && Date.today.month == task_config.month.to_i
             channel = Pinger.find_channel(server: server_name, channel: task_config.channel)
             channel.send_message(render(task_config.message))
+            count += 1
           end
         end
       end
     end
 
-    nil
+    count
   end
 
   private
