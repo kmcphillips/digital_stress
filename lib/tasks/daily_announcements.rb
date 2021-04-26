3bc8633275a86d48d205bc7b79f409ed9d59e4df
Kevin McPhillips <github@kevinmcphillips.ca>
2021-04-26 11:24:27 -0400

    More flexible way of defining daily announcements

--- lib/tasks/daily_announcements.rb
+++ lib/tasks/daily_announcements.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 class DailyAnnouncements < TaskBase
   def run
-    count = 0
+    results = []
 
+    # Config file defined tasks
     Global.config.servers.each do |server_name, server_config|
       if server_config.tasks&.daily_announcements
         server_config.tasks.daily_announcements.each do |task_config|
-          if Date.today.day == task_config.day.to_i && Date.today.month == task_config.month.to_i
-            channel = Pinger.find_channel(server: server_name, channel: task_config.channel)
-            channel.send_message(render(task_config.message))
-            count += 1
-          end
+          results << process_daily_announcement(task_config, server: server_name)
         end
       end
     end
 
-    count
+    results.select(&:present?).count
   end
 
   private
@@ -23,5 +20,25 @@ class DailyAnnouncements < TaskBase
   def render(message)
     template = ERB.new(message)
     template.result(binding) # TODO: expose a more useful binding here rather than counting on globals
+  end
+
+  def process_daily_announcement(task_config, server:)
+    if triggers_now?(task_config)
+      channel = Pinger.find_channel(server: server, channel: task_config.channel)
+      channel.send_message(render(task_config.message))
+
+      true
+    end
+  end
+
+  def triggers_now?(task_config)
+    if task_config.weekday || task_config.weekdays
+      days = Array(task_config.weekday || task_config.weekdays).compact.map(&:downcase)
+      days.include?(Date::DAYNAMES[Date.today.wday].downcase)
+    else
+      return false if task_config.year.present? && task_config.year.to_i != Date.today.year
+      return false if task_config.month.present? && task_config.month.to_i != Date.today.month
+      Date.today.day == task_config.day.to_i
+    end
   end
 end
