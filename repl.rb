ba68814a7b0ae71ebab2844087f9e27cdb64ac70
Kevin McPhillips <github@kevinmcphillips.ca>
2021-04-26 10:48:03 -0400

    Some repl improvements

--- repl.rb
+++ repl.rb
@@ -2,11 +2,20 @@
 require_relative "base"
 
 def run_bot
-  Global.bot = Discordrb::Bot.new(token: Global.config.discord.token)
-  Global.bot.run(true)
+  if !Global.bot
+    Global.bot = Discordrb::Bot.new(token: Global.config.discord.token)
+    Global.bot.run(true)
+  end
+  Global.bot
+end
+
+def test_channel
+  run_bot
+  Pinger.find_channel(server: 'duck-bot-test', channel: 'testing')
 end
 
 # The `Global` class has access to database, kv store, config, log, etc.
-# Do > run_bot
+# To connect to bot: run_bot
+# For the test channel: test_channel
 
 binding.pry
