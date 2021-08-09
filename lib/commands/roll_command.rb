b10e4c160284d657df879b254585ba2e00955af1
Kevin McPhillips <github@kevinmcphillips.ca>
2021-08-08 20:27:38 -0400

    Build out the roll command

--- lib/commands/roll_command.rb
+++ lib/commands/roll_command.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 class RollCommand < BaseCommand
   def response
-    dice = GamesDice.create(query)
-    dice.roll
-    ":d20: **#{ dice.result }** \n*(#{ dice.explain_result })*"
+    if query.blank?
+      "Quack! What do you want to roll? :d20:"
+    else
+      begin
+        str = query
+        str = "1#{ str }" if str =~ /^d[0-9]/
+        dice = GamesDice.create(str)
+        dice.roll
+        ":d20: **#{ dice.result }** \n*(#{ dice.explain_result })*"
+      rescue Parslet::ParseFailed => e
+        "Quack?? Not sure how to roll '#{ query }'!"
+      end
+    end
   end
 end
