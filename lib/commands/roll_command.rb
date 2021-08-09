21996527c732d17bf82d683cf2133b7de5f475fa
Kevin McPhillips <github@kevinmcphillips.ca>
2021-08-08 22:26:40 -0400

    Use the server emoji for the d20

--- lib/commands/roll_command.rb
+++ lib/commands/roll_command.rb
@@ -9,10 +9,16 @@ class RollCommand < BaseCommand
         str = "1#{ str }" if str =~ /^d[0-9]/
         dice = GamesDice.create(str)
         dice.roll
-        ":d20: **#{ dice.result }** \n*(#{ dice.explain_result })*"
+        "#{ d20_emoji } **#{ dice.result }** \n*(#{ dice.explain_result })*"
       rescue Parslet::ParseFailed => e
         "Quack?? Not sure how to roll '#{ query }'!"
       end
     end
+  end
+
+  private
+
+  def d20_emoji
+    event.server.emoji.values.find { |e| e.name == "d20" } || ":d20:"
   end
 end
