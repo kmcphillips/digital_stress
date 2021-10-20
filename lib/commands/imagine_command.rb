a2fab427a7c7fc352719eaab43dfd1666cb511c6
Kevin McPhillips <github@kevinmcphillips.ca>
2021-10-19 22:43:03 -0400

    Use a better prompt

--- lib/commands/imagine_command.rb
+++ lib/commands/imagine_command.rb
@@ -4,8 +4,19 @@ class ImagineCommand < BaseCommand
     if query.blank?
       "Quacking-imagine something"
     else
-      response = Global.openai_client.completions(engine: "davinci", parameters: { prompt: query, max_tokens: 40 })
-      response.parsed_response['choices'].map{ |c| c["text"] }
+      response = Global.openai_client.completions(engine: "davinci", parameters: { prompt: prompt(query), max_tokens: rand(50..75) })
+      result = response.parsed_response['choices'].map{ |c| c["text"] }
+      raise "Got a blank result: #{ response.parsed_response }" if result.blank?
+      result
     end
+  end
+
+  private
+
+  def prompt(text)
+    "This is an exercise in creativity. I'll give you an example, and you'll tell me a creative short story based on that example.
+
+    Example: #{ text }
+    Story: "
   end
 end
