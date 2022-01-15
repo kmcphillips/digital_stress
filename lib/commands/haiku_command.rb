fd7eecd8360e23ff9e3748fc704f38795d54ef0b
Kevin McPhillips <github@kevinmcphillips.ca>
2022-01-14 21:19:29 -0500

    Add haiku

---
+++ lib/commands/haiku_command.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+class HaikuCommand < BaseCommand
+  def response
+    if query.blank?
+      "Your input is blank\nI can't compose from nothing\nTry again quack quack"
+    else
+      OpenaiClient.completion(prompt(query), openai_params)
+    end
+  end
+
+  private
+
+  def prompt(text)
+    "Compose a haiku about #{ text.strip.gsub(/[.!?:;]\Z/, "") }."
+  end
+
+  def openai_params
+    {
+      engine: "davinci-instruct-beta-v3",
+      max_tokens: 64,
+      temperature: 0.9,
+      top_p: 1.0,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.5,
+    }
+  end
+end
