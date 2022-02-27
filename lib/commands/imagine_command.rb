4b032386e130985a61a612bee0c133a9cb1d05dd
Kevin McPhillips <github@kevinmcphillips.ca>
2022-02-27 16:41:47 -0500

    Allow some ai commands to be re-run

--- lib/commands/imagine_command.rb
+++ lib/commands/imagine_command.rb
@@ -8,6 +8,21 @@ class ImagineCommand < BaseCommand
     end
   end
 
+  def after(message:)
+    if message
+      Recorder.set_againable(
+        command_class: self.class.name,
+        query: query.strip,
+        query_user_id: event.author.id,
+        query_message_id: event.message.id,
+        response_user_id: message.author.id,
+        response_message_id: message.id,
+        server: server,
+        channel: channel,
+      )
+    end
+  end
+
   private
 
   def prompt(text)
