921c7fab3212e61d3f31de298b3ba92dc2186676
Kevin McPhillips <github@kevinmcphillips.ca>
2022-09-06 20:08:55 -0400

    Log openai request

--- lib/clients/openai_client.rb
+++ lib/clients/openai_client.rb
@@ -4,6 +4,7 @@ module OpenaiClient
 
   def completion(prompt, openai_params)
     parameters = openai_params.symbolize_keys
+    Global.logger.info("[OpenaiClient] request #{ parameters } prompt: #{ prompt }")
     engine = parameters.delete(:engine).presence || "davinci"
     parameters[:prompt] = prompt
     response = Global.openai_client.completions(engine: engine, parameters: parameters)
