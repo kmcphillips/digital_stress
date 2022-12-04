69f1990d3109c74691c05a24fb8644aad3aa6f7f
Kevin McPhillips <github@kevinmcphillips.ca>
2022-12-03 21:22:41 -0500

    Add dalle wrapper

--- lib/clients/openai_client.rb
+++ lib/clients/openai_client.rb
@@ -10,16 +10,27 @@ module OpenaiClient
     Global.openai_client.models.list["data"].map { |m| m["id"] }
   end
 
-  def completion(prompt, openai_params)
+  def completion(prompt, openai_params={})
     parameters = openai_params.symbolize_keys
-    Global.logger.info("[OpenaiClient] request #{ parameters } prompt:\n#{ prompt }")
-    raise "OpenaiClient: passed in `engine` param, use `model` instead" if parameters.key?(:engine)
+    Global.logger.info("[OpenaiClient][completion] request #{ parameters } prompt:\n#{ prompt }")
+    raise "[OpenaiClient][completion] passed in `engine` param, use `model` instead" if parameters.key?(:engine)
     parameters[:model] ||= OpenaiClient.default_model
     parameters[:prompt] = prompt
     response = Global.openai_client.completions(parameters: parameters)
-    Global.logger.info("[OpenaiClient] response #{ response.inspect }")
+    Global.logger.info("[OpenaiClient][completion] response #{ response.inspect }")
     result = response.parsed_response["choices"].map{ |c| c["text"] }
-    raise "Got a blank result: #{ response.parsed_response }" if result.blank?
+    raise "[OpenaiClient][completion] request #{ parameters } prompt:\n#{ prompt } gave a blank result: #{ response.parsed_response }" if result.blank?
+    result
+  end
+
+  def image(prompt, openai_params={})
+    parameters = openai_params.symbolize_keys
+    Global.logger.info("[OpenaiClient][image] request #{ parameters } prompt:\n#{ prompt }")
+    parameters[:prompt] = prompt
+    response = Global.openai_client.images.generate(parameters: parameters)
+    Global.logger.info("[OpenaiClient][image] response #{ response.inspect }")
+    result = response.parsed_response["data"].map{ |c| c["url"] }
+    raise "[OpenaiClient][image] request #{ parameters } prompt:\n#{ prompt } gave a blank result: #{ response.parsed_response }" if result.blank?
     result
   end
 end
