6f719c6a36f1166dfc953eb47048b72b3040167f
Kevin McPhillips <github@kevinmcphillips.ca>
2022-12-04 21:21:12 -0500

    Add AI recipe image

--- lib/commands/recipe_command.rb
+++ lib/commands/recipe_command.rb
@@ -6,25 +6,39 @@ class RecipeCommand < BaseCommand
     if query.blank?
       "What kind of recipe?"
     else
-      OpenaiClient.completion(prompt(query), openai_params).first.strip
+      file = nil
+      publication = publications.sample
+
+      thread = Thread.new do
+        file = Dreamstudio.image_file(image_prompt(query, publication: publication))
+      end
+
+      text = OpenaiClient.completion(text_prompt(query, publication: publication), openai_params).first.strip
+
+      thread.join
+      event.send_file(file, filename: "recipe.png") if file
+
+      text
     end
   end
 
   private
 
-  def prompt(text)
-    x = "Write a recipe for \"#{ text.strip }\" appearing in #{ publication }"
-    puts x
-    x
+  def text_prompt(text, publication:)
+    "Write a recipe for \"#{ text.strip }\" appearing in #{ publication }"
   end
 
-  def publication
+  def image_prompt(text, publication:)
+    "Recipe book photo of \"#{ text.strip }\" as it would appear in #{ publication }"
+  end
+
+  def publications
     [
       "Bon Appetit",
       "Cook's Illustrated",
       "Modernist Cuisine",
       "Betty Crocker’s Cookbook",
-    ].sample
+    ]
   end
 
   def openai_params
