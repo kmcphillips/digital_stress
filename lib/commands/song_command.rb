e4ccc40211335e47401614ec5bdeab6fde9ccb1d
Kevin McPhillips <github@kevinmcphillips.ca>
2022-12-07 08:14:02 -0500

    Sing command

---
+++ lib/commands/song_command.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+class SongCommand < BaseCommand
+  include AfterRecorderStrikethroughAgainable
+
+  def response
+    if query.blank?
+      "What should we sing about?"
+    else
+      OpenaiClient.completion(prompt(query), openai_params).first.strip
+    end
+  end
+
+  private
+
+  def prompt(text)
+    text = "about #{ text.strip }" unless text.strip.downcase.start_with?("about")
+    "Write the lyrics to the chorus of a hit song #{ text.strip }."
+  end
+
+  def openai_params
+    {
+      model: OpenaiClient.default_model,
+      max_tokens: 256,
+      temperature: 1.0,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    }
+  end
+end
