60e0a903823a3d2459e10215a4cebf1f7c507f18
Kevin McPhillips <github@kevinmcphillips.ca>
2022-12-16 22:13:18 -0500

    Allow dalle to return blob file rather than URL

--- lib/clients/openai_client.rb
+++ lib/clients/openai_client.rb
@@ -35,12 +35,28 @@ module OpenaiClient
     response = Global.openai_client.images.generate(parameters: parameters)
     Global.logger.info("[OpenaiClient][image] response #{ response.inspect }")
     if response.success?
-      result = response.parsed_response["data"].map{ |c| c["url"] }
-      raise "[OpenaiClient][image] request #{ parameters } prompt:\n#{ prompt } gave a blank result: #{ response.parsed_response }" if result.blank?
-      result
+      if parameters[:response_format] == "b64_json"
+        result = response.parsed_response["data"].map{ |c| c["b64_json"] }
+        raise "[OpenaiClient][image] request #{ parameters } prompt:\n#{ prompt } gave a blank b64_json result: #{ response.parsed_response }" if result.blank?
+        result
+      else
+        result = response.parsed_response["data"].map{ |c| c["url"] }
+        raise "[OpenaiClient][image] request #{ parameters } prompt:\n#{ prompt } gave a blank url result: #{ response.parsed_response }" if result.blank?
+        result
+      end
     else
       error_message = response.parsed_response["error"] rescue nil
       ":bangbang: OpenAI returned error HTTP #{ response.code } #{ error_message }"
+    end
+  end
+
+  def image_file(prompt, openai_params={})
+    parameters = openai_params.symbolize_keys.merge(response_format: "b64_json")
+    image(prompt, parameters).map do |b64_json|
+      file = Tempfile.create(["dalle", ".png"], binmode: true)
+      file.write(Base64.decode64(b64_json))
+      file.rewind
+      file
     end
   end
 end
