c84c115f41cca58970a7b5a3e34e5e1a261457ab
Kevin McPhillips <github@kevinmcphillips.ca>
2022-12-16 21:27:11 -0500

    Auto image prompt generation for game

--- lib/commands/new_command.rb
+++ lib/commands/new_command.rb
@@ -10,7 +10,30 @@ class NewCommand < BaseSubcommand
 
 
   def game
-    OpenaiClient.completion(game_prompt(query), openai_params).first.strip
+    result = OpenaiClient.completion(game_prompt(query), openai_params).first.strip
+
+    image_prompt = nil
+
+    result = result.lines.map do |line|
+      if matches = line.match(/\{(.+)\}/)
+        image_prompt = matches[1].gsub(/ai image prompt:/i, "").strip
+
+        nil
+      else
+        line
+      end
+    end.compact.join("\n")
+
+    begin
+      if image_prompt.present? && image_prompt.length > 10
+        file = Dreamstudio.image_file(image_prompt)
+        event.send_file(file, filename: "game.png") if file
+      end
+    rescue => e
+      result = "#{ result }\n:bangbang: #{ e.message }"
+    end
+
+    result
   end
 
   private
@@ -24,7 +47,7 @@ class NewCommand < BaseSubcommand
 
     review_sentiment = [ "positive", "negative", "mixed" ].sample
 
-    "Think of an imaginary video game. The game should #{ about_fragment }have some bizarre or nonsensical elements, and at least four-player multiplayer. Tell me the name, tagline, and a one-sentence description of the game. Then tell me the price, platforms, and metacritic score. Then give a review for the game written in a #{ review_sentiment } tone."
+    "Think of an imaginary video game. The game should #{ about_fragment }have some bizarre or nonsensical elements, and at least four-player multiplayer. Tell me the name, tagline, and a one-sentence description of the game. Then tell me the price, platforms, and metacritic score. Then give a review for the game written in a #{ review_sentiment } tone. Then give me an AI image prompt for the box art of the game, wrapped in {}."
   end
 
   def openai_params
