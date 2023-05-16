4b1522a2112513125a48fa1efd812a716db601b1
Kevin McPhillips <github@kevinmcphillips.ca>
2023-05-16 09:32:21 -0400

    Remove our question models

--- lib/responders/ai_responder.rb
+++ lib/responders/ai_responder.rb
@@ -14,28 +14,24 @@ class AiResponder < BaseResponder
       completion("Play a popular song using only kitchen noises.")
     end
 
-    respond_match(/\?\Z/i, chance: 0.05) do
-      key = "ai_responder_question:#{ server }:#{ channel }"
-
-      if !Global.kv.read(key).present? # TODO: this could be promoted into a `frequency:` option that does kv read/write
-        Global.kv.write(key, Time.now.to_i.to_s, ttl: 5.minutes)
-        start_typing
-        from_name = guess_names_from_text(text).sample || MandateModels::QUESTION_MODELS.keys.sample
-
-        name, completion = MandateModels.question(text, name: from_name)
-
-        if completion.blank?
-          name, completion = MandateModels.question(text, name: from_name)
-        end
-
-        if completion.blank?
-          "Quack! After two attempts at n=2 all responses came back blank for question completion of #{ from_name }: #{ text }"
-        else
-          "> **#{ name.capitalize }**: #{ completion.gsub("\n", "\n> ") }"
-        end
-
-      end
-    end
+    # respond_match(/\?\Z/i, chance: 0.05) do
+    #   key = "ai_responder_question:#{ server }:#{ channel }"
+    #
+    #   if !Global.kv.read(key).present? # TODO: this could be promoted into a `frequency:` option that does kv read/write
+    #     Global.kv.write(key, Time.now.to_i.to_s, ttl: 5.minutes)
+    #     start_typing
+    #     from_name = guess_names_from_text(text).sample || MandateModels::QUESTION_MODELS.keys.sample
+    #
+    #     name, completion = MandateModels.question(text, name: from_name)
+    #     name, completion = MandateModels.question(text, name: from_name) if completion.blank?
+    #
+    #     if completion.blank?
+    #       ":bangbang: Quack! After two attempts at n=2 all responses came back blank for question completion of #{ from_name }: #{ text }"
+    #     else
+    #       "> **#{ name.capitalize }**: #{ completion.gsub("\n", "\n> ") }"
+    #     end
+    #   end
+    # end
   end
 
   private
