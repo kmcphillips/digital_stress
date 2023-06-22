be58ec151570626433d2628addc2d8a5dc1bbf44
Kevin McPhillips <github@kevinmcphillips.ca>
2023-06-22 13:34:08 -0400

    richard

--- lib/responders/simple_responder.rb
+++ lib/responders/simple_responder.rb
@@ -4,6 +4,7 @@ class SimpleResponder < BaseResponder
 
   MANDATE_CHANNELS = [
     "mandatemandate#general",
+    "mandatemandate#wives",
     "duck-bot-test#testing",
   ].freeze
 
@@ -19,7 +20,8 @@ class SimpleResponder < BaseResponder
     respond_match(/\bheat\b/i, "don't be hwat...", chance: 0.08)
     respond_match(/several people are typing/i, "https://i.kym-cdn.com/photos/images/newsfeed/001/249/060/9c3.gif")
     respond_match(/safe/i, "https://i.imgur.com/1WReL2h.jpg", channels: MANDATE_CHANNELS, chance: 0.02)
-    respond_match(/\Afuck ./i, "You talking to me?", channels: MANDATE_CHANNELS, chance: 0.3)
+    respond_match(/\Afuck(\Z| .)/i, "You talking to me?", channels: MANDATE_CHANNELS, chance: 0.3)
+    respond_match(/\Adick(\Z| .)/i, "Call me Richard", channels: MANDATE_CHANNELS)
     respond_match(/pickle.?surprise/i, "https://cdn.discordapp.com/attachments/250717538063745024/1054566918536843274/Eliot_ultimate_pickle_surprise_8bf66f6a-b3a1-441d-a530-241e7c37d8b5.png")
   end
 end
