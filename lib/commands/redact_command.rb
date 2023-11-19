b5771eab266ecb04dec326cbb1e82da2c0d4a06f
Kevin McPhillips <github@kevinmcphillips.ca>
2023-11-18 21:12:15 -0500

    Redact from DMs

--- lib/commands/redact_command.rb
+++ lib/commands/redact_command.rb
@@ -4,15 +4,24 @@ class RedactCommand < BaseCommand
 
   def response
     if event.channel.pm?
-      "Quack, I can't redact DMs."
-    elsif !Recorder.record_channel?(server: server, channel: channel)
+      server_channel_token, redact_time = query.split(" ", 2)
+      redact_server, redact_channel = server_channel_token.split("#", 2)
+
+      return "Quack, can't parse server#channel from '#{ server_channel_token }'." if redact_server.blank? || redact_channel.blank?
+    else
+      redact_server = server
+      redact_channel = channel
+      redact_time = query
+    end
+
+    if !Recorder.record_channel?(server: redact_server, channel: redact_channel)
       "Quack, I can't redact a channel I am not recording."
     else
-      minutes = if query.blank?
+      minutes = if redact_time.blank?
         DEFAULT_MINUTES
-      elsif matches = query.match(/(\d+\s?m)/)
+      elsif matches = redact_time.match(/(\d+\s?m)/)
         matches[1].to_i
-      elsif matches = query.match(/(\d+\s?h)/)
+      elsif matches = redact_time.match(/(\d+\s?h)/)
         matches[1].to_i * 60
       end
 
@@ -21,9 +30,13 @@ class RedactCommand < BaseCommand
       elsif minutes <= 0
         "Quack, I can't redact #{ minutes } minutes."
       else
-        deleted = Recorder.delete_last_minutes(minutes, server: server, channel: channel)
+        deleted = Recorder.delete_last_minutes(minutes, server: redact_server, channel: redact_channel)
 
-        ":pause_button: Quack, redacted #{ minutes } minutes of messages (#{ deleted.count } messages)."
+        if event.channel.pm?
+          ":pause_button: Quack, redacted #{ minutes } minutes of messages in **#{ redact_server }##{ redact_channel }** (#{ deleted.count } messages)."
+        else
+          ":pause_button: Quack, redacted #{ minutes } minutes of messages (#{ deleted.count } messages)."
+        end
       end
     end
   end
