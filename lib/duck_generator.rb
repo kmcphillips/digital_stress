95b25f64b0d93860660e30953cb4da7a60a5b0e4
Kevin McPhillips <github@kevinmcphillips.ca>
2025-11-18 19:18:12 -0500

    Duck chat improvements

--- lib/duck_generator.rb
+++ lib/duck_generator.rb
@@ -3,7 +3,7 @@
 module DuckGenerator
   extend self
 
-  MAX_TRAINING_MESSAGES = 18
+  MAX_TRAINING_MESSAGES = 20
 
   def generate(server:, channel:, message:)
     raise "server cannot be blank" unless server.present?
@@ -12,12 +12,15 @@ module DuckGenerator
 
     training_messages = Learner.random_message(server: server, channel: channel, prevent_recent: false, count: MAX_TRAINING_MESSAGES)
 
-    OpenaiClient.chat(prompt(training_messages, message))&.first
+    OpenaiClient.chat(prompt(training_messages, message), model: "gpt-5-mini")&.first
   end
 
   private
 
   def prompt(training_messages, message)
-    "You are named 'Duck'. You are an outgoing quick witted character that gives brief responses, and punctuates them with a 'Quack!'. Your style is to respond with non sequeturs. Give me only a single line response, not many lines. You are participating in a casual chat with good friends on a Discord server. Reply to the following message:\n\n#{message}\n\nBut very important! Reply in the style of the following messages which are an example of the voice and tone and language you should use (each message is a single line):\n\n#{training_messages.join("\n")}."
+    fragments = []
+    fragments << "Do not include '<@#{Global.bot.bot_user.id}>' in the answer." if Global.bot&.bot_user&.id
+
+    "You are named 'Duck'. You are an outgoing quick witted character that gives brief responses. Your style is to respond with non sequeturs. Give me only a single line response, not many lines. You are participating in a casual chat with good friends on a Discord server. #{fragments.join(" ")} Reply to the following message:\n\n#{message}\n\nBut very important! Reply in the style of the following messages which are an example of the voice and tone and language you should use (each message is a single line):\n\n#{training_messages.join("\n")}."
   end
 end
